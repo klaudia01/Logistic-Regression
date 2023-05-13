b3c8c212e1b93bfe8774753aa44674d1cc6f4aca
k_bar <k_barabasz@outlook.com>
2023-05-13 23:49:20 +0200

    Update comments

--- experiments.py
+++ experiments.py
@@ -17,17 +17,18 @@ from sklearn.tree import DecisionTreeClassifier
 from logistic_regression import LogisticRegression
 
 X, y = make_classification(
-        n_samples=1000,
-        n_features=15,
-        n_informative=5,
-        n_classes=2,
-        weights=[0.9, 0.1],
-        random_state=42
+    n_samples=1000,
+    n_features=15,
+    n_informative=5,
+    n_classes=2,
+    weights=[0.9, 0.1],
+    random_state=42
 )
 # zapisanie wygenerowanych danych syntetycznych do pliku CSV
 dataset = np.concatenate((X, y[:, None]), axis=1)
 np.savetxt('dataset.csv', dataset, delimiter=',')
 
+# inicjalizacja walidacji krzyżowej
 rskf = RepeatedStratifiedKFold(n_splits=2, n_repeats=5, random_state=42)
 
 # Eksperyment 1: Znalezienie optymalnego hiperparametru liczby iteracji
@@ -39,23 +40,28 @@ iterations_list = [100, 200, 500, 1000, 2000, 5000, 10000]
 scores = np.zeros((10, 7))
 
 for i, (train_index, test_index) in enumerate(rskf.split(X, y)):
-        X_train, X_test = X[train_index], X[test_index]
-        y_train, y_test = y[train_index], y[test_index]
+    # Podział danych na zbiór treningowy i testowy
+    X_train, X_test = X[train_index], X[test_index]
+    y_train, y_test = y[train_index], y[test_index]
 
-        for i_iterations, iterations in enumerate(iterations_list):
-                lg = LogisticRegression(learning_rate=0.01, iterations=iterations)
-                lg.fit(X_train, y_train)
-                y_pred = lg.predict(X_test)
-                scores[i, i_iterations] = accuracy_score(y_test, y_pred)
+    for i_iterations, iterations in enumerate(iterations_list):
+        # inicjalizacja modelu regresji logistycznej
+        lg = LogisticRegression(learning_rate=0.01, iterations=iterations)
+        # trenowanie modelu i predykcja etykiet
+        lg.fit(X_train, y_train)
+        y_pred = lg.predict(X_test)
+        scores[i, i_iterations] = accuracy_score(y_test, y_pred)
 
-        mean_scores = np.round(np.mean(scores, axis=0), 3)
+    # wyznaczanie średniego wyniku metryki
+    mean_scores = np.round(np.mean(scores, axis=0), 3)
 
-        # znalezienie optymalnej wartości liczby iteracji
-        optimal_iterations = iterations_list[np.argmax(mean_scores)]
+    # znalezienie optymalnej wartości liczby iteracji
+    optimal_iterations = iterations_list[np.argmax(mean_scores)]
 
-        df = pd.DataFrame({'Liczba iteracji': iterations_list,
-                           'Dokładność': [mean_scores[0], mean_scores[1], mean_scores[2], mean_scores[3], mean_scores[4],
-                                          mean_scores[5], mean_scores[6]]})
+    # wyświetlanie wyników
+    df = pd.DataFrame({'Liczba iteracji': iterations_list,
+                       'Dokładność': [mean_scores[0], mean_scores[1], mean_scores[2], mean_scores[3], mean_scores[4],
+                                      mean_scores[5], mean_scores[6]]})
 
 print(df, '\n\nOptymalna liczba iteracji: ', optimal_iterations, '\n')
 
@@ -73,26 +79,33 @@ scores_3 = []
 pca = PCA(n_components=6)
 
 for i, (train, test) in enumerate(rskf.split(X, y)):
-        X_train, X_test = X[train_index], X[test_index]
-        y_train, y_test = y[train_index], y[test_index]
+    # Podział danych na zbiór treningowy i testowy
+    X_train, X_test = X[train_index], X[test_index]
+    y_train, y_test = y[train_index], y[test_index]
 
-        pca.fit(X_train)
-        X_train = pca.transform(X_train)
-        X_test = pca.transform(X_test)
+    # trenowanie algorytmu KBest
+    pca.fit(X_train)
+    # transformacja danych treningowych i testowych
+    X_train = pca.transform(X_train)
+    X_test = pca.transform(X_test)
 
-        lg = LogisticRegression(learning_rate=0.01, iterations=optimal_iterations)
-        lg.fit(X_train, y_train)
-        y_pred = lg.predict(X_test)
+    # inicjalizacja modelu regresji logistycznej
+    lg = LogisticRegression(learning_rate=0.01, iterations=optimal_iterations)
+    # trenowanie modelu i predykcja etykiet
+    lg.fit(X_train, y_train)
+    y_pred = lg.predict(X_test)
 
-        accuracy = accuracy_score(y_test, y_pred)
-        scores = np.append(scores, accuracy)
+    # wyznaczanie wyników metryk
+    accuracy = accuracy_score(y_test, y_pred)
+    scores = np.append(scores, accuracy)
 
-        precision = precision_score(y_test, y_pred)
-        scores_2.append(precision)
+    precision = precision_score(y_test, y_pred)
+    scores_2.append(precision)
 
-        recall = recall_score(y_test, y_pred)
-        scores_3.append(recall)
+    recall = recall_score(y_test, y_pred)
+    scores_3.append(recall)
 
+# wyznaczanie średnich wyników metryk
 mean_accuracy_PCA = np.round(np.mean(scores), 3)
 mean_precision_PCA = np.round(np.mean(scores_2), 3)
 mean_recall_PCA = np.round(np.mean(scores_3), 3)
@@ -104,24 +117,30 @@ np.savez('PCA_results.npz', scores, scores_2, scores_3)
 KBest = SelectKBest(k=int(np.sqrt(X.shape[1])))
 
 for i, (train, test) in enumerate(rskf.split(X, y)):
-        X_train, X_test = X[train_index], X[test_index]
-        y_train, y_test = y[train_index], y[test_index]
+    # Podział danych na zbiór treningowy i testowy
+    X_train, X_test = X[train_index], X[test_index]
+    y_train, y_test = y[train_index], y[test_index]
 
-        KBest.fit(X_train, y_train)
+    # trenowanie algorytmu KBest
+    KBest.fit(X_train, y_train)
 
-        lg = LogisticRegression(learning_rate=0.01, iterations=optimal_iterations)
-        lg.fit(X_train, y_train)
-        y_pred = lg.predict(X_test)
+    # inicjalizacja modelu regresji logistycznej
+    lg = LogisticRegression(learning_rate=0.01, iterations=optimal_iterations)
+    # trenowanie modelu i predykcja etykiet
+    lg.fit(X_train, y_train)
+    y_pred = lg.predict(X_test)
 
-        accuracy = accuracy_score(y_test, y_pred)
-        scores = np.append(scores, accuracy)
+    # wyznaczanie wyników metryk
+    accuracy = accuracy_score(y_test, y_pred)
+    scores = np.append(scores, accuracy)
 
-        precision = precision_score(y_test, y_pred)
-        scores_2.append(precision)
+    precision = precision_score(y_test, y_pred)
+    scores_2.append(precision)
 
-        recall = recall_score(y_test, y_pred)
-        scores_3.append(recall)
+    recall = recall_score(y_test, y_pred)
+    scores_3.append(recall)
 
+# wyznaczanie średnich wyników metryk
 mean_accuracy_KBest = np.round(np.mean(scores), 3)
 mean_precision_KBest = np.round(np.mean(scores_2), 3)
 mean_recall_KBest = np.round(np.mean(scores_3), 3)
@@ -129,6 +148,7 @@ mean_recall_KBest = np.round(np.mean(scores_3), 3)
 # zapisanie wyników do pilku
 np.savez('KBest_results.npz', scores, scores_2, scores_3)
 
+# wyświetlanie wyników
 df = pd.DataFrame({'Metoda preprocessingu': ['PCA', 'KBest'],
                    'Dokładność': [mean_accuracy_PCA, mean_accuracy_KBest],
                    'Precyzja': [mean_precision_PCA, mean_precision_KBest],
@@ -152,17 +172,23 @@ classifiers = [LogisticRegression(learning_rate=0.01, iterations=optimal_iterati
                ]
 
 for i, (train_index, test_index) in enumerate(rskf.split(X, y)):
-        X_train, X_test = X[train_index], X[test_index]
-        y_train, y_test = y[train_index], y[test_index]
+    # Podział danych na zbiór treningowy i testowy
+    X_train, X_test = X[train_index], X[test_index]
+    y_train, y_test = y[train_index], y[test_index]
 
-        for i_classifier, classifier in enumerate(classifiers):
-                clf = clone(classifier)
-                clf.fit(X_train, y_train)
-                y_pred = clf.predict(X_test)
-                scores[i, i_classifier] = accuracy_score(y_test, y_pred)
-                scores_2[i, i_classifier] = precision_score(y_test, y_pred)
-                scores_3[i, i_classifier] = recall_score(y_test, y_pred)
+    for i_classifier, classifier in enumerate(classifiers):
+        # tworzenie kopii klasyfikatora
+        clf = clone(classifier)
+        # trenowanie modelu i predykcja etykiet
+        clf.fit(X_train, y_train)
+        y_pred = clf.predict(X_test)
 
+        # wyznaczanie wyników metryk
+        scores[i, i_classifier] = accuracy_score(y_test, y_pred)
+        scores_2[i, i_classifier] = precision_score(y_test, y_pred)
+        scores_3[i, i_classifier] = recall_score(y_test, y_pred)
+
+# wyznaczanie średnich wyników metryk
 mean_scores = np.round(np.mean(scores, axis=0), 3)
 mean_scores_2 = np.round(np.mean(scores_2, axis=0), 3)
 mean_scores_3 = np.round(np.mean(scores_3, axis=0), 3)
@@ -170,9 +196,11 @@ mean_scores_3 = np.round(np.mean(scores_3, axis=0), 3)
 # zapisanie wyników do pilku
 np.savez('reference_methods_results.npz', scores, scores_2, scores_3)
 
+# wyświetlanie wyników
 df = pd.DataFrame({'Klasyfikator': ['LR', 'DT', 'GaussianNB', 'KNN', 'RL SKlearn'],
                    'Dokładność': [mean_scores[0], mean_scores[1], mean_scores[2], mean_scores[3], mean_scores[4]],
-                   'Precyzja': [mean_scores_2[0], mean_scores_2[1], mean_scores_2[2], mean_scores_2[3], mean_scores_2[4]],
-                   'Czułość': [mean_scores_3[0], mean_scores_3[1], mean_scores_3[2], mean_scores_3[3], mean_scores_3[4]]})
-
+                   'Precyzja': [mean_scores_2[0], mean_scores_2[1], mean_scores_2[2], mean_scores_2[3],
+                                mean_scores_2[4]],
+                   'Czułość': [mean_scores_3[0], mean_scores_3[1], mean_scores_3[2], mean_scores_3[3],
+                               mean_scores_3[4]]})
 print(df)
